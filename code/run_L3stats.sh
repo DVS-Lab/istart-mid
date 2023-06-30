b2277ea279f5876f468f2385de95dcb330b3ac9d
Jimmy Wyngaarden <tun46412@cla18994.tu.temple.edu>
2023-06-30 11:32:03 -0400

    clean up files

--- code/run_L3stats.sh
+++ code/run_L3stats.sh
@@ -10,8 +10,8 @@ maindir="$(dirname "$scriptdir")"
 
 
 # this loop defines the different types of analyses that will go into the group comparisons
-for analysis in ppi_seed-NAcc; do # act nppi-dmn nppi-ecn ppi_seed | type-${type}_run-01
-	for INTerm in wOutInt; do #  wInt wOutInt running with and without Interactions interaction were correlated with main effects?
+for analysis in act ppi_seed-NAcc; do # act nppi-dmn nppi-ecn ppi_seed | type-${type}_run-01
+	for INTerm in 4A 4B 4C 4D  4E-VBeta 4E-LL 4E-LG 4F-VBeta 4F-LL 4F-LG 4G-VBeta 4G-LL 4G-LG; do #  wInt wOutInt running with and without Interactions interaction were correlated with main effects?
 		analysistype=type-${analysis}
 
 		# these define the cope number (copenum) and cope name (copename)
